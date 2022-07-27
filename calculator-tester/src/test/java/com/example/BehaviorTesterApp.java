bbfe8a224e125a3ec8af665d28a5c3a81f85511b
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2022-07-27 23:21:03 +0200

    Deletes not necessary class

--- calculator-tester/src/test/java/com/example/BehaviorTesterApp.java
+++ calculator-tester/src/test/java/com/example/BehaviorTesterApp.java
@@ -1,11 +1,23 @@
 package com.example;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
+
 @SpringBootApplication
 @EnableConfigurationProperties
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
 public class BehaviorTesterApp {
 
     public static void main(String[] args) {
